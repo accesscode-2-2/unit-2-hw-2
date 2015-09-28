73bef46814b119e6bc5a5d2793fdb4a2a00ddc0f
mikekavouras <kavourasm@gmail.com>
2015-09-28 13:21:27 -0400

    hw for this week

---
+++ hw-week-2/hw-week-2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hw-week-2
+//
+//  Created by Michael Kavouras on 9/28/15.
+//  Copyright © 2015 Michael Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
