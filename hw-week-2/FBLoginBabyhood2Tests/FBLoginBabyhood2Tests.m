032e30d882290245ba50508a641e316e72d5c309
ayunav <ayunanyc@gmail.com>
2015-10-02 15:34:55 -0400

    added files to hw folder

---
+++ hw-week-2/FBLoginBabyhood2Tests/FBLoginBabyhood2Tests.m
@@ -0,0 +1,39 @@
+//
+//  FBLoginBabyhood2Tests.m
+//  FBLoginBabyhood2Tests
+//
+//  Created by Ayuna Vogel on 9/28/15.
+//  Copyright © 2015 Ayuna Vogel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FBLoginBabyhood2Tests : XCTestCase
+
+@end
+
+@implementation FBLoginBabyhood2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ hw-week-2/Podfile
@@ -0,0 +1,2 @@
+pod 'FBSDKLoginKit', '~> 4.6'
+pod 'FBSDKCoreKit', '~> 4.6'
